03479c8bcf7c5c04e4d6e3972866a826348cf791
Sachin Agarwal <sachin@bigbitbus.com>
2018-11-08 21:54:07 +0000

    Made a small change

--- django-poll-project/workflow.sh
+++ django-poll-project/workflow.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 OLD_IMAGE_HASH=`git rev-parse --short HEAD`
+echo "Original git head revision at: "
+git rev-parse --short HEAD
 
 # Commit changes
 git commit -a -m "Made a small change"
@@ -11,7 +13,13 @@ NEW_IMAGE_HASH=`git rev-parse --short HEAD`
 docker build --tag kubernetes101/django_image:$NEW_IMAGE_HASH .
 
 # Replace old image by new image
-sed -i -e 's/$OLD_IMAGE_HASH/$NEW_IMAGE_HASH/g' ../kubecode/deployment_django.yaml
+sed -i -e "s/$OLD_IMAGE_HASH/$NEW_IMAGE_HASH/g" ../kubecode/deployment_django.yaml
+
+
+# Apply (current context, namespace) in development/test
+kubectl apply -f ../kubecode/deployment_django.yaml
+
+#Run tests here
 
 # Optional - push your code to git
 # git push
@@ -20,6 +28,4 @@ sed -i -e 's/$OLD_IMAGE_HASH/$NEW_IMAGE_HASH/g' ../kubecode/deployment_django.ya
 # docker login 
 # docker push kubernetes101/django_image:$NEW_IMAGE_HASH
 
-# Apply (current context, namespace)
-kubectl apply -f ../kubecode/deployment_django.yaml
 
